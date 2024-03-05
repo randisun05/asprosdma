d33d775e6384044afa67e5485b32742caacd0260
randi sunardi <randisunardi1995@gmail.com>
2024-03-05 14:38:43 +0700

    tambahan feature

--- resources/views/Layouts/Components/About.blade.php
+++ resources/views/Layouts/Components/About.blade.php
@@ -9,7 +9,7 @@
 		<div style="text-align: justify;">
                <p class="justified-text">Atas berkat rahmat Tuhan Yang Mahasa Esa, bangsa Indonesia dalam mengisi kemerdekaan dapat hidup damai dalam tatanan global yang dinasmis, dengan memanfaatkan seluruh sumber daya yang dimiliki guna mewujudkan masyarakat yang adil, makmur dan sejahtera berdasarkan Pancasila dan Undang-Undang Dasar 1945. Untuk itu diperlukan upaya pembaruan yang terus menerus, baik secara perorangan, kelompok, dan kerjasama yang didorong keyakinan serta tanggung jawab untuk ikut serta dalam meningkatkan kualitas aparatur, sebagai pengawal pembangunan. Mengingat dinamika masyarakat Indonesia, paradigma pelayanan publik ditumbuhkembangkan agar dapat memenuhi kewajiban dasar aparatur dalam melaksanakan tugasnya. Jabatan fungsional Sumber Daya Manusia Aparatur (SDMA) sebagai insan Aparatur Pemerintah, ikut bertanggung jawab untuk mengembangkan melalui pengayaan ilmu pengetahuan dan teknologi yang selaras dengan nilai moral dan etika bangsa Indonesia, sehingga dalam pemberian pelayanan publik bersikap profesional.</p>
               	</div>
-		            <a href="/tentang-aspro" class="button btnprimary top20 pagescroll">Selengkapnya</a>
+                  <a href="/tentang-aspro" class="button btnsecondary">Selengkapnya</a>
             </div>
          </div>
          <div class="col-md-5 col-sm-5 padding_bottom wow fadeInRight" data-wow-delay="350ms">
