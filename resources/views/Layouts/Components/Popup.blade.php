48e5b8366113697aafd266f9089d6b51212f37be
randisun05 <randisun1995@gmail.com>
2024-10-22 06:58:02 +0700

    update pop-up

--- resources/views/Layouts/Components/Popup.blade.php
+++ resources/views/Layouts/Components/Popup.blade.php
@@ -8,19 +8,47 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-
                 <div class="cbp-item web logo">
-                    <img src="/assets/images/pop-up-kombel-3.jpg" alt="" style="width: 100%">
-                        <div class="overlay center-block whitecolor">
-                            {{-- <a class="plus" target="_blank" href="https://asprosdma.id/events/%22komunitas-belajar%22-aspro-sdma-series-3-tentang-sayembara-literasi-sdm-aparatur-berkarya-untuk-negeri"></a> --}}
-                        <a class="plus" data-fancybox="" href="/assets/images/pop-up-kombel-3.jpg"></a>
+                {{-- //slider --}}
+                    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                          <div class="carousel-item active">
+                            <img src="/assets/images/pop-up-rini-1.jpg" class="d-block w-100" alt="...">
+                            <div class="overlay center-block whitecolor">
+                                <a class="plus" data-fancybox="" href="/assets/images/pop-up-rini-1.jpg"></a>
+                            </div>
+                          </div>
+
+                          <div class="carousel-item">
+                            <img src="/assets/images/pop-up-purwadi-2.jpg" class="d-block w-100" alt="...">
+                            <div class="overlay center-block whitecolor">
+                                <a class="plus" data-fancybox="" href="/assets/images/pop-up-purwadi-2.jpg"></a>
+                                </div>
+                          </div>
+
                         </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                          <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                          <span class="visually-hidden">Next</span>
+                        </button>
+                      </div>
+
+                      {{-- // single image --}}
+                    {{-- <img src="/assets/images/pop-up-kombel-3.jpg" alt="" style="width: 100%">
+                        <div class="overlay center-block whitecolor">
+                        <a class="plus" data-fancybox="" href="/assets/images/pop-up-kombel-3.jpg"></a>
+                        </div> --}}
                 </div>
 
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <a href="https://asprosdma.id/events/%22komunitas-belajar%22-aspro-sdma-series-3-tentang-sayembara-literasi-sdm-aparatur-berkarya-untuk-negeri" class="btn btn-primary" target="_blank">Detail</a>
+        {{-- //optional button --}}
+        {{-- <a href="https://asprosdma.id/events/%22komunitas-belajar%22-aspro-sdma-series-3-tentang-sayembara-literasi-sdm-aparatur-berkarya-untuk-negeri" class="btn btn-primary" target="_blank">Detail</a> --}}
       </div>
     </div>
   </div>
