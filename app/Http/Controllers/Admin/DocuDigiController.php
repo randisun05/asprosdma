3d2cdfb30df72582199b83e0ff65036b05e448a4
randisunardi1995@gmail.com <randisunardi1995@gmail.com>
2025-07-12 21:26:56 +0700

    update role sekre

--- app/Http/Controllers/Admin/DocuDigiController.php
+++ app/Http/Controllers/Admin/DocuDigiController.php
@@ -39,7 +39,7 @@ class DocuDigiController extends Controller
         ->paginate(10);
         $docus->appends(['q' => request()->q]);
 
-        if (auth()->user()->role == ['administrator', 'sekretaris']) {
+        if (in_array(auth()->user()->role, ['administrator', 'sekretaris'])) {
             $docus = DocumentDigital::
             when(request()->q, function($query) {
                 $query->where('perihal', 'like', '%' . request()->q . '%');
