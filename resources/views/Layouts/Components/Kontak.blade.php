aa05424008566756072b6a9eeb70fe715b998e1a
randisunardi1995@gmail.com <randisunardi1995@gmail.com>
2025-10-15 15:14:52 +0700

    update kontak

--- resources/views/Layouts/Components/Kontak.blade.php
+++ resources/views/Layouts/Components/Kontak.blade.php
@@ -14,7 +14,7 @@
             <p>Kantor Badan Kepegawaian Negara Pusat, Gedung II, Lantai 6, Jl. Mayor Jendral Sutoyo No.12 4, RT.4/RW.14, Cililitan, Kec. Kramat jati, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13640 </p>
                <div class="col-md-6 col-sm-12 our-address top20">
                   <h5>Telepon</h5>
-                  <p class="bottom15"><span><p class="bottom15"><span><a href="https://api.whatsapp.com/send/?phone=6282319192189&text&type=phone_number&app_absent=0">Iceu wa.me/6282319192189</a>
+                  <p class="bottom15"><span><p class="bottom15"><span><a href="https://api.whatsapp.com/send/?phone=6282319192189&text&type=phone_number&app_absent=0">Admin Aspro SDMA wa.me/6282319192189</a>
                   </span> </p>
                </div>
                <div class="col-md-6 col-sm-6 our-address top20">
