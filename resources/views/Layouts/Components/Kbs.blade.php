a5098fa5e2e83e416899ffc79d0bae1818be7c7a
randisunardi1995@gmail.com <randisunardi1995@gmail.com>
2025-10-15 14:55:20 +0700

    update kbs bkn

--- resources/views/Layouts/Components/Kbs.blade.php
+++ resources/views/Layouts/Components/Kbs.blade.php
@@ -1,6 +1,6 @@
 <!--Registration -->
 
-<section id="our-feature" class="padding_bottom padding_top_half single-feature">
+<section id="our-feature" class="padding_bottom single-feature">
    <div class="container">
     <div class="row">
          <div class="col-md-12 col-sm-12 text-center">
@@ -13,13 +13,13 @@
          <div class="col-md-6 col-sm-7 text-md-start text-center wow fadeInLeft" data-wow-delay="300ms">
             <div class="heading-title mb-4">
 
-               <h3 class="darkcolor">Butuh panduan resmi seputar layanan ASN dan SIASN?</h3>
+               <h3 class="darkcolor">Butuh panduan resmi seputar layanan ASN ?</h3>
             </div>
-            <p class="mb-4">Sekarang tak perlu bingung lagi. Badan Kepegawaian Negara (BKN) telah menyediakan Knowledge Base BKN, portal pengetahuan digital yang berisi kumpulan panduan, SOP, video tutorial, template SK, hingga peraturan terbaru semuanya dalam satu tempat mudah diakses.</p>
+            <p class="mb-4">Sekarang tak perlu bingung lagi. Badan Kepegawaian Negara (BKN) telah menyediakan Knowledge Base Sistem, portal pengetahuan digital yang berisi kumpulan panduan, SOP, video tutorial, template SK, hingga peraturan terbaru semuanya dalam satu tempat mudah diakses.</p>
              <p class="mb-4">Melalui halaman resmi <a href="https://support-siasn.bkn.go.id/kbs">support-siasn.bkn.go.id/kbs</a>, ASN dan instansi pemerintah dapat menemukan berbagai informasi praktis untuk menunjang pengelolaan data kepegawaian secara lebih cepat, transparan, dan efisien.</p>
          </div>
          <div class="col-md-6 col-sm-5 wow fadeInRight" data-wow-delay="300ms">
-            <div class="image top100"  style="width:100%"><img alt="SEO" src="/assets/images/kbs.png" stayle="width:70%"></div>
+            <div class="image top100"  style="width:100%"><img alt="SEO" src="/assets/images/kbs.png" style="width:100%;"></div>
          </div>
       </div>
    </div>
