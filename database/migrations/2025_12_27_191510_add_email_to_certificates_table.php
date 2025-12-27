a5bbac655e5424adb25b5070941e07493aa8943e
randisunardi1995@gmail.com <randisunardi1995@gmail.com>
2025-12-27 19:35:07 +0700

    tambah kolom email instansi

---
+++ database/migrations/2025_12_27_191510_add_email_to_certificates_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('certificates', function (Blueprint $table) {
+            $table->string('email')->nullable()->after('name');
+            $table->string('instansi')->nullable()->after('name');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('certificates', function (Blueprint $table) {
+            $table->dropColumn('email');
+            $table->dropColumn('instansi');
+        });
+    }
+};
